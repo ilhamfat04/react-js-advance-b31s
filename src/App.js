f580dd1de494a486bba4f578bb2e0741b36e6d99
= <=>
2022-03-02 15:58:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import DetailUser from "./pages/DetailUser";
 // import component here
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
   return (
@@ -30,7 +31,10 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/signin" element={<SignIn />} />
-        {/* change route for page about, profile, and user:id to private route */}
+        <Route exact path="/" element={<PrivateRoute />}>
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </Router>
   );
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,4 +1,13 @@
 // import necessary utility from rrd
+import { useState } from "react";
+import { Outlet, Navigate } from "react-router-dom";
 
+const PrivateRoute = () => {
+    const [isLogin, setIsLogin] = useState(false)
 
-// create component here
+    return (
+        isLogin ? <Outlet /> : <Navigate to="/signin" />
+    )
+}
+
+export default PrivateRoute
