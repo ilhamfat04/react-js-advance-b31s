d2824eadbff63b7d26b3604f3d5f3ad6d91dbb3b
= <=>
2022-03-02 15:07:45 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // import necessary object from react-router-dom
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 // import "pages" component here
+import About from './pages/About'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
 
 function App() {
   return (
     // define <BrowserRouter> as parent element
-    <>
+    <Router>
       <div>
-        {/* Setup navigation here */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" >Home</Link>
+            </li>
+            <li>
+              <Link to="/about" >About</Link>
+            </li>
+            <li>
+              <Link to="/profile" >Profile</Link>
+            </li>
+            <li>
+              <Link to="/signin" >Sign In</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
       {/* code <Routes> and <Route> below, according to path and component */}
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </Router>
   );
 }
 
