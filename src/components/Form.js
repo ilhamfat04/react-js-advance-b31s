cf49688168a6da3d4506246cd2cdf6633c971ffb
= <=>
2022-03-02 13:46:00 +0700

    update:

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,29 @@
 // create style here
+const styles = {
+  form: {
+    backgroundColor: "red",
+    margin: "15px 10px",
+  }
+}
 
 // create Form component
 function Form() {
   return (
-    // code inside div
     <div>
-      {/* form */}
+      <form style={styles.form} >
+        <div>
+          <label htmlFor="username"
+            style={{ display: "block", marginBottom: "5" }}>
+            Username
+          </label>
+          <input type="text"
+            id="username"
+            style={{
+              fontSize: "1rem",
+              padding: "5px"
+            }} />
+        </div>
+      </form>
     </div>
   )
 }
