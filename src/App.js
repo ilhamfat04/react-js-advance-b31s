7d888389799c0c8222bfb060054083b2e4994fe9
= <=>
2022-03-02 15:42:17 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 // import component here
+import DetailUser from "./pages/DetailUser";
 
 function App() {
   return (
@@ -35,6 +36,7 @@ function App() {
         <Route exact path="/profile" element={<Profile />} />
         <Route exact path="/signin" element={<SignIn />} />
         {/* define new route */}
+        <Route exact path="/user/:id" element={<DetailUser />} />
       </Routes>
     </Router>
   );
--- src/pages/DetailUser.js
+++ src/pages/DetailUser.js
@@ -2,15 +2,19 @@ import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 
 // import necessary hooks from react-router-dom 
+import { useParams } from "react-router-dom";
 
 const DetailUser = () => {
   const [data, setData] = useState(null);
-  
+
   // call that hooks here and store to variable
+  const params = useParams()
+
+  console.log(params);
 
   // don't worry about this, we'll cover later
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${}`)
+    fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
       .then((response) => response.json())
       .then((json) => setData(json));
     return () => {
@@ -23,6 +27,11 @@ const DetailUser = () => {
     // display information that stored in data 
     // console.log first to see what information that you get from fetching
     <Container className="text-center">
+      <h1>Data User</h1>
+      <br />
+      <p>{data?.name}</p>
+      <p>{data?.username}</p>
+      <p>{data?.email}</p>
     </Container>
   );
 };
